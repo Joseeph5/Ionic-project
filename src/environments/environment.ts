93113a2efb73babdf575188549bd0b5929b6325a
Youssef Benamor <youssef.beenamor@gmail.com>
2024-10-20 11:53:05 +0100

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyALEM9qpwrVG7AxgS5TxlDZomOJz2VgnIo',
+    authDomain: 'ionic-project-27627.firebaseapp.com',
+    projectId: 'ionic-project-27627',
+    storageBucket: 'ionic-project-27627.appspot.com',
+    messagingSenderId: '1037658117269',
+    appId: '1:1037658117269:web:9a3a0a2a2509bf52ef779c',
+    measurementId: 'G-3QFZT6G7RT',
+  },
 };
 
 /*
