87f0b2fa1f26d7d5e6616b3642bca88f286e5060
Youssef Benamor <youssef.beenamor@gmail.com>
2024-10-24 11:46:13 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyALEM9qpwrVG7AxgS5TxlDZomOJz2VgnIo',
+    authDomain: 'ionic-project-27627.firebaseapp.com',
+    projectId: 'ionic-project-27627',
+    storageBucket: 'ionic-project-27627.appspot.com',
+    messagingSenderId: '1037658117269',
+    appId: '1:1037658117269:web:9a3a0a2a2509bf52ef779c',
+    measurementId: 'G-3QFZT6G7RT',
+  },
 };
